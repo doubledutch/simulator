0e571951dffbbacdd4fa6aedb464c4e8acd5b409
Adam Liechty <adam@doubledutch.me>
2019-04-16 15:47:05 -0700

    + react-native-image-picker

--- android/app/src/main/java/com/extensionsimulator/MainApplication.java
+++ android/app/src/main/java/com/extensionsimulator/MainApplication.java
@@ -3,6 +3,7 @@ package com.extensionsimulator;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.chirag.RNMail.RNMail;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new AsyncStoragePackage(),
             new RNCWebViewPackage(),
             new RNMail(),
